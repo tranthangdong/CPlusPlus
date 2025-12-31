c5aff611c127be96558c9f7da7d537da76e4e56d
tranthangdong <tranthangdong@gmail.com>
2025-12-31 15:35:29 +0700

    Cập nhật ví dụ
    Ví dụ 4.11

--- vidu_chuong4/vidu4.11.cpp
+++ vidu_chuong4/vidu4.11.cpp
@@ -1,9 +1,38 @@
-#include "SPI.h"
-pinMode(ss, OUTPUT);
-SPI.begin();
-SPI.setBitOrder(MSBFIRST);
-SPI.setBitOrder(LSBFIRST);
-		
-digitalWrite(SS, LOW);
-SPI.transfer(value);
-digitalWrite(ss, HIGH);
+#include <SD.h>
+#define SD_ChipSelectPin 4
+#include <TMRpcm.h>
+#include <SPI.h>
+	 
+TMRpcm tmrpcm;
+unsigned long time = 0;
+	 
+void setup()
+{
+ 
+		tmrpcm.speakerPin = 9 
+		Serial.begin(9600); //bật serial monitor ở mức baudrate 9600
+		if (!SD.begin(SD_ChipSelectPin))
+		{
+			Serial.println("SD fail");
+			return;
+		}else{
+			Serial.println("OK");
+		}
+	
+		tmrpcm.play("xinchao.wav");
+		delay(3000);
+		time = millis();
+	} 
+	void loop()
+	{
+		tmrpcm.play("tambiet.wav");
+		delay(5500);
+		digitalWrite(9, LOW);
+	 
+		if(millis() - time > 50)
+		{
+			if (!tmrpcm.isPlaying())
+			digitalWrite(9, LOW);
+			time = millis();
+		}
+}
